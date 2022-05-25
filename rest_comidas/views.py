e43ce2c08760983eecfd0ea28d34df21f7c86fb1
TecaFondo <carlos.kohler.r@mail.pucv.cl>
2022-05-25 14:46:35 -0400

    se agregan funciones de edicion en api

--- rest_comidas/views.py
+++ rest_comidas/views.py
@@ -24,3 +24,24 @@ def list_productos(request):
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+@api_view(['GET','PUT','DELETE'])
+def detalle_productos(request,id):
+    try:
+        producto =Producto.objects.get(cod_prod = id)
+    except Producto.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        serializer = ProductoSerializer(producto)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        dataP = JSONParser().parse(request)
+        serializer = ProductoSerializer(producto, data = dataP)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        producto.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
