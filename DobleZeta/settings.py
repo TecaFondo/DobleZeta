9f67a39bfe15e47b86912d162809e2e92265ce87
TecaFondo <carlos.kohler.r@mail.pucv.cl>
2022-06-06 15:13:02 -0400

    Se agrega autentificacion de usuario para api

--- DobleZeta/settings.py
+++ DobleZeta/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'restaurant',
     'rest_comidas',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +72,12 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 WSGI_APPLICATION = 'DobleZeta.wsgi.application'
 
