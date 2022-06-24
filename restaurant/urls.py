79d5fe951193dd550abcf01c89dd0212a4c780eb
TecaFondo <carlos.kohler.r@mail.pucv.cl>
2022-06-24 18:58:21 -0400

    Se realizan modificaciones

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,13 +2,13 @@
 #from tkinter import Menu
 from unicodedata import name
 from django.urls import path
-from restaurant.views import menu, index, user_login, newUser, recuperar,carga, form_mod_producto,vista_admin,form_del_producto
+from restaurant.views import cerrarsesion, menu, index, user_login, newUser, recuperar,carga, form_mod_producto,vista_admin,form_del_producto
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns =[
-    path('',index, name='index'),
+    path('',user_login, name='index'),
     path('index/',index, name='index'),
     path('menu/', menu, name='menu'),
     path('login/', user_login, name='login'),
@@ -18,6 +18,7 @@ urlpatterns =[
     path('editar/<id>',form_mod_producto,name='form_mod_producto'),
     path('vista_admin/',vista_admin,name='vista_admin'),
     path('borrar-prod/<id>',form_del_producto,name='form_del_producto'),
+    path('logout/',cerrarsesion,'cerrarsesion')
     #path('form-productos',form_productos,name='form_productos'),   
 ]
 if settings.DEBUG:
